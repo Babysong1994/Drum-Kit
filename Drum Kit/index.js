500d227b4b2a38e2c99818baa69f3ebbcc530acc
Agnes0823 <sykkkk0032@163.com>
2020-06-22 15:26:00 +0800

    Add files via upload

---
+++ Drum Kit/index.js
@@ -0,0 +1,51 @@
+// Detecting Button Press
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < numberOfDrumButtons; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+    });    
+}
+
+// Detecting Keyboard Press
+document.addEventListener("keydown", function(event){
+    makeSound(event.keyCode);
+});
+
+
+function makeSound(keyCode) {
+    switch (keyCode) {
+        case 87:
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        break;
+        case 65:
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        break;
+        case 83:
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        break;
+        case 68:
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        break;
+        case 74:
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+        break;
+        case 75:
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+        break;
+        case 76:
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+        break;
+    }
+    
+    
+}
